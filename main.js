55603b98d5d0e6eb7da532e00381b14a70332236
NIGHTINGALE1411 <92319401+NIGHTINGALE1411@users.noreply.github.com>
2022-04-26 14:55:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+var  blockimageobject=""
+
+function new_image()
+{ fabric.Image.fromURL("BirthdayImage.jpg",function(Img){
+    blockimageobject=Img;
+    blockimageobject.scaleToWidth(700)
+    blockimageobject.scaleToHeight(510)
+    blockimageobject.set({
+        top:0,left:0
+
+    }
+     );
+     canvas.add(blockimageobject)
+})
+	
+	
+}
+
+function playSound(){
+x.play()	
+}
